c63170dc68def9c569654ab5b80231beafaa3795
Paweł Zaremba <47338686+PeZet2@users.noreply.github.com>
2022-07-16 13:07:13 +0200

    wykorzystanie argparse oraz gitignore

--- github_backuper.py
+++ github_backuper.py
@@ -1,34 +1,32 @@
 #!/usr/bin/env python3
-
 #############################
 ## https://github.com/PeZet2
 #############################
-
 import os
 import datetime
-from urllib.request import urlopen
+import argparse
 from pathlib import Path
 from github import Github
+from urllib.request import urlopen
 
-USER="PeZet2"
-access_token=""
 
-home = f"{Path.home()}"
-archive_dir=f"{home}/archive"
-download_dir=f"{archive_dir}/github"
-download_repo_list_file=f"{download_dir}/repo_list.txt"
-log_dir = f"{home}/log"
-log_file=f"{log_dir}/github_backuper.log"
+def parse_args():
+    parser = argparse.ArgumentParser(description='Parse input parameters')
+    parser.add_argument('--user',
+                        type=str,
+                        help='Github username'
+                        )
+    parser.add_argument('--access_token',
+                        type=str,
+                        help='Access token for private repositories'
+                        )
+    return parser.parse_args()
 
-g = Github(access_token)
 
-master_branch="master"
-main_branch="main"
-
-def saveLog(stri, ts=True):
+def save_log(stri, ts=True):
     tm = datetime.datetime.now()
     strtm = tm.strftime("%Y-%m-%d %H:%M:%S")
-    if ts == True:
+    if ts:
         message = f"[{strtm}]: {stri}"
     else:
         message = f"{stri}"
@@ -37,65 +35,79 @@ def saveLog(stri, ts=True):
 
 
 def create_dir(a_dir: str):
-    if (not os.path.isdir(a_dir)):
+    if not os.path.isdir(a_dir):
         os.mkdir(a_dir)
 
 
-def get_repo_list() -> list:
-    saveLog(f"Fetching repos list from GitHub...")
-    return [r for r in g.get_user().get_repos()]
+def get_repo_list(github: Github) -> list:
+    save_log(f"Fetching repos list from GitHub...")
+    return [r for r in github.get_user().get_repos()]
 
 
 def delete_file(file_name: str):
-    if (os.path.isfile(file_name)):
+    if os.path.isfile(file_name):
             os.remove(file_name)
 
 
 def save_repo_to_a_file(repo_list: list, repo_list_file: str):
     delete_file(repo_list_file)
     with open(repo_list_file, 'at') as f:
-        saveLog(f"Saving download urls for all repos to {repo_list_file}...")
+        save_log(f"Saving download urls for all repos to {repo_list_file}...")
         for repo in repo_list:
             f.write(repo.get_archive_link('zipball') + "\n")
-    saveLog("Urls saved")
+    save_log("Urls saved")
 
 
-def download_repo(repo_name:str, url:str):
-    saveLog(f"Downloading repository: {repo_name}...")
+def download_repo(repo_name: str, url: str):
+    save_log(f"Downloading repository: {repo_name}...")
 
     response = urlopen(url)
     zip_filename = download_dir + '/' + repo_name + '.zip'
 
     with open(zip_filename, 'wb') as f:
         f.write(response.read())
-    saveLog(f"Saved repo to {zip_filename}")
+    save_log(f"Saved repo to {zip_filename}")
 
 
+def main():
+    # Utworzenie obiektu Github wraz z otrzymanym tokenem
+    g = Github(args.access_token)
 
-#Utworzenie lokalizacji dla logow
-create_dir(log_dir)
+    # Utworzenie lokalizacji dla logow
+    create_dir(log_dir)
 
-# Utworzenie lokalizacji archiwum
-create_dir(archive_dir)
-create_dir(download_dir)
+    # Utworzenie lokalizacji archiwum
+    create_dir(archive_dir)
+    create_dir(download_dir)
 
-saveLog("###########################################################", ts=False)
-saveLog("####################################")
-saveLog(f"Creating GitHub backup for {USER}")
+    save_log("###########################################################", ts=False)
+    save_log("####################################")
+    save_log(f"Creating GitHub backup for {args.user}")
 
-# Pobranie listy repozytoriów z GitHub
-repo_list=get_repo_list()
-saveLog(f"Repositories: {len(repo_list)}")
-if (len(repo_list) == 0):
-    saveLog("ERR: No repositories to backup!!!")
-    exit(1)
+    # Pobranie listy repozytoriów z GitHub
+    repo_list = get_repo_list(g)
+    save_log(f"Repositories: {len(repo_list)}")
+    if len(repo_list) == 0:
+        save_log("ERR: No repositories to backup!!!")
+        exit(1)
 
-# Zapisanie linków pobierających repozytoria do pliku
-save_repo_to_a_file(repo_list, download_repo_list_file)
+    # Zapisanie linków pobierających repozytoria do pliku
+    save_repo_to_a_file(repo_list, download_repo_list_file)
 
-# Pobieranie repozytoriów do archiwum
-for r in repo_list:
-    download_repo(r.name, r.get_archive_link('zipball'))
+    # Pobieranie repozytoriów do archiwum
+    for r in repo_list:
+        download_repo(r.name, r.get_archive_link('zipball'))
+
+    save_log("End of script")
 
 
-saveLog("End of script")
+if __name__ == "__main__":
+    home = f"{Path.home()}"
+    archive_dir = f"{home}/archive"
+    download_dir = f"{archive_dir}/github"
+    download_repo_list_file = f"{download_dir}/repo_list.txt"
+    log_dir = f"{home}/log"
+    log_file = f"{log_dir}/github_backuper.log"
+
+    args = parse_args()
+    main()
